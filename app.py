fe74e4f278d822630355a2cb83d958e0fcc01a49
Chirag Jhumkhawala <chirag912@gmail.com>
2022-09-21 00:58:48 +0530

    reduced fields, ignore the previous commit

--- app.py
+++ app.py
@@ -8,7 +8,19 @@ import streamlit as st
 
 #Title
 st.title('ETL Demo')
-st.write('CSV to Json')
+st.write("""
+         
+### Tutorial :-
+
+* Add Left Padding = **Feature Name, Name for New Feature, No of Padding You Want.**   
+ 
+* Sub String = **Feature Name, Name for New Splitted Feature, Starting Number From Where You Want Split, Number From Where You Want to Stop Split.**    
+  
+       
+        """)
+
+
+st.write('CSV to Json/CSV')
 
 
 # Upload the file
@@ -21,6 +33,8 @@ def convert_df(df):
 
 # Function to Split a Feature
 def split(Feature_Name, New_Name1, New_Name2):
+    if Feature_Name == New_Name1:
+        st.error("Enter the name different from feature name")
     df = pd.read_csv('Transformed_20220419_new_orders_to_rpac.csv')
     df[[New_Name1, New_Name2]] = df[Feature_Name].str.split(",", 1, expand = True)
     csv = df.to_csv('Transformed_20220419_new_orders_to_rpac.csv', index = None)
@@ -52,6 +66,7 @@ def apply_pad(PFeature_name, New_Name, NOS):
     try:
         df = pd.read_csv('Transformed_20220419_new_orders_to_rpac.csv')
         if PFeature_name == New_Name:
+            #pass
             st.error("Enter the name different from feature name")
         L = add_pad(df[PFeature_name], NOS)
         df[New_Name] = L
@@ -62,6 +77,7 @@ def apply_pad(PFeature_name, New_Name, NOS):
     except:
         df = pd.read_csv("20220419_new_orders_to_rpac.csv")
         if PFeature_name == New_Name:
+            #pass
             st.error("Enter the name different from feature name")
         L = add_pad(df[PFeature_name], NOS)
         df[New_Name] = L
@@ -86,7 +102,8 @@ def apply_custom_splits(SFeature_name, SNew_Name, Start, End):
     try:
         df = pd.read_csv('Transformed_20220419_new_orders_to_rpac.csv')
         if SFeature_name == SNew_Name:
-            st.error("Enter the name different from feature name")
+            pass
+            # st.error("Enter the name different from feature name")...
         L = custom_splits(df[SFeature_name], Start, End)
         df[SNew_Name] = L
         csv = df.to_csv('Transformed_20220419_new_orders_to_rpac.csv', index = None)
@@ -96,7 +113,8 @@ def apply_custom_splits(SFeature_name, SNew_Name, Start, End):
     except:
         df = pd.read_csv("20220419_new_orders_to_rpac.csv")
         if SFeature_name == SNew_Name:
-            st.error("Enter the name different from feature name")
+            pass
+            # st.error("Enter the name different from feature name")...
         L = custom_splits(df[SFeature_name], Start, End)
         df[SNew_Name] = L
         csv = df.to_csv('Transformed_20220419_new_orders_to_rpac.csv', index = None)
@@ -108,15 +126,53 @@ def apply_custom_splits(SFeature_name, SNew_Name, Start, End):
 st.sidebar.title('User Controls')
 
 st.sidebar.write('Input for Padding Function')
-PFeature_Name = st.sidebar.text_input(label = 'Enter the Feature Name Which You Want to Add Padding')
-New_Name = st.sidebar.text_input(label = 'Enter the Name for New Feature')
-NOS  = st.sidebar.text_input(label = 'Enter the No of Padding You Want')
 
+
+# st.markdown("""
+# <style>
+# .stTextInput > label {
+# font-size:100%; 
+# font-weight:bold; 
+# color:white; 
+# } 
+# </style>
+# """, unsafe_allow_html=True)
+
+# What i try.
+
+PFeature = st.sidebar.text_input(label ='Enter the Feature Name Which You Want to Add Padding. Eg:Hrid,Hrid_new,4')
+pFeaturelist = PFeature.split(",")
+
+if len(pFeaturelist) == 3:
+        PFeature_Name =pFeaturelist[0]
+        New_Name = pFeaturelist[1]
+        NOS  = pFeaturelist[2]
+        
+elif len(pFeaturelist) > 3:
+        st.error('Please Enter Vaild Input Value', icon="🚨")
+        PFeature_Name = None
+        New_Name = None
+        NOS  = None
+
+# What i try.
+    
 st.sidebar.write('Input for Custom Split Function')
-SFeature_name  = st.sidebar.text_input(label = 'Enter the Feature Name Which You Want to Custom Split')
-SNew_Name = st.sidebar.text_input(label = 'Enter the Name for New Splitted Feature')
-Start = st.sidebar.text_input(label = 'Enter the Starting Number From Where You Want Split')
-End = st.sidebar.text_input(label = 'Enter the End Number From Where You Want to Stop Split')
+SFeature  = st.sidebar.text_input(label = 'Enter the Feature Name Which You Want to Sub String. Eg:Hrid,Hrid_new,0,4')
+SFeaturelist = SFeature.split(",")
+
+if len(SFeaturelist) == 4:
+    SFeature_name  = SFeaturelist[0]
+    SNew_Name = SFeaturelist[1]
+    Start = SFeaturelist[2]
+    End = SFeaturelist[3]
+    
+elif len(SFeaturelist) > 4:
+        st.error('Please Enter Vaild Input Value', icon="🚨")
+        SFeature_name  = None
+        SNew_Name = None
+        Start = None
+        End = None
+       
 
 if uploaded_file is not None:
     #Converting the file into DataFrame
@@ -160,7 +216,7 @@ if uploaded_file is not None:
             #Download
             csv_dwnld = convert_df(df)
             st.download_button("Press to Download", csv_dwnld,"Transformed.csv","text/csv",key='download-csv')
-            st.write()
+            st.write("""#### Kindly Download the CSV file to check the actual result of padding""")
 
         except ValueError:
             st.error("Please Input Number Value")
@@ -171,7 +227,7 @@ if uploaded_file is not None:
         except AttributeError:
             pass
     
-    if st.sidebar.button("ADD a Padding"):
+    if st.sidebar.button("ADD Left Padding"):
         try:
             df = apply_pad(PFeature_Name, New_Name, NOS)
             st.markdown('Padded CSV')
@@ -179,38 +235,54 @@ if uploaded_file is not None:
             #Download
             csv_dwnld = convert_df(df)
             st.download_button("Press to Download", csv_dwnld,"Transformed.csv","text/csv",key='download-csv')
+            st.write("""##### Kindly Download the CSV file to check the actual result of padding""")
 
         except ValueError:
             st.error("Please Input Number Value")
+            
+            
+        except NameError:
+            # st.error("Please Input Number Value")
+            st.error('Please Enter Vaild Input', icon="🚨")
+            
 
         except KeyError:
             st.error("Feature Does not Exist")
+            #st.error("Please Input Number Value")...
 
         except AttributeError:
             pass
 
-    if st.sidebar.button("ADD Custom Split"):
+    if st.sidebar.button("ADD Sub String"):
         try:
             df = apply_custom_splits(SFeature_name, SNew_Name, Start, End)
-            st.markdown('Custom Splited CSV')
+            st.markdown('Sub String CSV')
             st.write(df.head())
             #Download
             csv_dwnld = convert_df(df)
             st.download_button("Press to Download", csv_dwnld,"Transformed.csv","text/csv",key='download-csv')
 
+
         except ValueError:
             st.error("Please Input Number Value")
+            
+        except NameError:
+            st.error("Please Input Correct Value")
+            #st.error('Please Enter Vaild Input', icon="🚨")
 
         except KeyError:
             st.error("Feature Does not Exist")
+            # st.error("Feature Does not Exist")...
 
         except AttributeError:
             pass
 
+
     try:
         with open("20220419_new_orders_to_rpac.json", "r") as f:
-            data_columns = json.load(f)
-            st.write(data_columns[:1])
+            pass
+            # data_columns = json.load(f)    # comment
+            # st.write(data_columns[:1])     # comment
 
     except:
         pass
